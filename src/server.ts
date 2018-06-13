007a62cce7fafe04ac8759461841d188e0ace2cd
Kenneth <k.aasan@sportradar.com>
2018-06-12 20:06:22 -0700

    Enable introspection

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { ApolloServer } from 'apollo-server';
 import { resolvers } from './resolvers';
 import { typeDefs } from './typeDefs';
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers, introspection: true });
 
 server.listen().then(({ url }) => {
   // tslint:disable-next-line no-console
